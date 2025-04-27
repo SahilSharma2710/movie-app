89e9d0e9ed75f324dfee429ac04e52d65ee8c705
sahilsharma <sahilsharma@geekyants.com>
2025-04-27 18:26:18 +0530

    feat: upated the credentional for backend

--- mobile/lib/core/constants/api_url.dart
+++ mobile/lib/core/constants/api_url.dart
@@ -1,8 +1,7 @@
 class ApiUrl {
-
-  static const baseURL = 'https://movie-backend-production-8413.up.railway.app/'; 
+  static const baseURL = 'http://localhost:3000/';
   static const apiV = 'api/v1/';
-  
+
   static const signup = '${apiV}auth/signup';
   static const signin = '${apiV}auth/signin';
   static const trendingMovies = '${apiV}movie/trending';
